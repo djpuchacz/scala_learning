6cb4b5d8c376840ebe4afc6362bcf69c76b1f1f8
Lukasz Wozniak <lukaszwozniak@outlook.com>
2023-07-05 09:37:59 +0200

    intr oop

--- src/main/scala/lectures/part2oop/OOBasics.scala
+++ src/main/scala/lectures/part2oop/OOBasics.scala
@@ -1,5 +1,26 @@
 package lectures.part2oop
 
 object OOBasics extends App {
+  val person = new Person("John", 26)
+  println(person.age)
+  person.greet("Bruno")
 
+  class Person(name: String, val age: Int) { //constructor
+    // body
+    val x = 2 // można używać tego pola jako pola klasy
+    println(1+3) //expression
+
+    //method
+    def greet(name: String): Unit = println(s"${this.name} says: Hi, $name")
+
+    //overloading
+    def greet(): Unit = println(s"Hi, I'm $name")
+
+    //multiple constructors
+    def this(name: String) = this(name, 0) //lepiej dodać default value to głównego konstruktora(linijka 8), niż tutaj )do pomocniczego konstruktora)
+  }
+
+  //poprzez dodanie słowa kluczowego val do konstrultora klasy możemy go "drukować" tak jak w Javie
+
+  //class parameters are NOT Fields
 }
